7bba152e4305d7696cea21ce55a1969ae71c8ce1
大橘大利 <8541215+big-orange-and-big-profit@user.noreply.gitee.com>
2021-02-24 16:30:30 +0800

    添加项目文件。

---
+++ KZW.Web/Controllers/GoodsWebController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KZW.Web.Controllers
+{
+    public class GoodsWebController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ KZW.Web/Controllers/MemberWebController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KZW.Web.Controllers
+{
+    public class MemberWebController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
